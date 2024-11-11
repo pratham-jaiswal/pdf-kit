064fe763d26e81d84915512baab39e18f5f8fb49
pratham-jaiswal <prathamj0502@gmail.com>
2024-11-12 01:57:39 +0530

    update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'assets', 'icon.ico'),
+    icon: path.join(__dirname, 'resources', 'icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
